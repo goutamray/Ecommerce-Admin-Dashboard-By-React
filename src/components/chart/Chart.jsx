b03fb351d5b9fcf0ef577541d89ac468188d52d3
goutam <goutamr362@gmail.com>
2024-04-19 10:46:59 +0600

    crm page design done

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,74 @@
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);   
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+
+  },
+};
+
+
+
+const Chart = () => {
+
+  const labels = ['January', 'February', 'March', 'April', "May", "June", "July", "Augest", "September", "October", "November", "December"];
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Campaign Sent',
+        data: [3, 6, 8, 6, 8, 4, 1,5, 2, 7, 4, 1 ],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Leads Generate',
+        data: [4, 7, 2, 1, 9, 2, 7, 4, 3, 1, 9, 6],
+        backgroundColor: '#4094f1',
+
+      },
+      {
+        label: 'Deals Customer',
+        data: [1, 9, 5, 3, 6, 1, 6, 3, 9],
+        backgroundColor: '#de2fff',
+
+      },
+      {
+        label: 'Revenue Booked',
+        data: [ 6, 3, 9, 1, 5, 3, 6, 1, 4, 2, 4, 3, 1],
+        backgroundColor: '#ff8c0a',
+  
+      },
+    ],
+  };
+
+  return (
+    <>
+       <Bar options={options} data={data} />
+    </>
+  )
+}
+
+export default Chart
+
--- src/pages/crm/Crm.jsx
+++ src/pages/crm/Crm.jsx
@@ -1,7 +1,12 @@
 
+import { FaAngleLeft, FaAngleRight, FaRegEye, FaTrashAlt } from "react-icons/fa";
+import { BiSolidDownload } from "react-icons/bi";
 
+import goutam from "../../assets/img/goutam.png"
 
 import "./Crm.css"; 
+import Chart from "../../components/chart/Chart";
+import { HiDotsHorizontal } from "react-icons/hi";
 const Crm = () => {
   return (
     <>
@@ -21,7 +26,276 @@ const Crm = () => {
           </div>
         </div>
 
+        <div className="row my-3">
+          <div className="col-sm-12">
+            <div className="ppp-chart">
+              <div className="card p-3">
+                <div className="top-part">
+                  <h4> Selling Overview </h4>
+                  <p><HiDotsHorizontal /></p>
+                </div>
+                <div className="single-chart">
+                      <Chart /> 
+                </div>
+              </div>  
+            </div>
+          </div>
         </div>
+
+
+        <div className="row my-4">
+          <div className="col-sm-12">
+            <div className="my-chart">
+              <div className="card shadow p-3"> 
+
+               <div className="top-part">
+                 <h4> Deals_performance</h4>
+                 <p><HiDotsHorizontal /></p>
+               </div>
+
+            <div className="row cartFilter">
+               <div className="col-sm-3 col-single">
+                  <h4 > SHOW BY </h4>
+                  <select className="form-select" aria-label="Default select example">
+                      <option selected value="1">12 Row </option>
+                      <option value="2">24 Row </option>
+                      <option value="3">36 Row </option>
+                </select>
+               </div>
+               <div className="col-sm-3 col-single">
+                  <h4 > STATUS BY </h4>
+                  <select className="form-select" aria-label="Default select example">
+                      <option selected value="1"> Won Leads </option>
+                      <option value="2"> New Leads </option>
+                      <option value="3"> Open Leads  </option>
+                      <option value="3"> Lost Leads</option>
+                </select>
+               </div>
+               <div className="col-sm-3 col-single">
+                  <h4 > BRAND BY </h4>
+                  <select className="form-select" aria-label="Default select example">
+                      <option selected value="1"> Ecstasy</option>
+                      <option value="2"> Freeland </option>
+                      <option value="3"> Rongdhonu </option>
+                </select>
+               </div>
+               <div className="col-sm-3 col-single">
+                  <h4 >SEARCH BY </h4>
+                  <div className="search">
+                    <input type="search" placeholder="id / name / category / brand"/>
+                  </div>
+               </div>
+           </div>
+
+
+              <div className="table-responsive">
+              <table className="table table-bordered table-striped">
+                <thead >
+                    <tr className="table-primary">
+                      <th> S.L </th>
+                      <th> REPRESENTER </th>
+                      <th> EMAIL </th>
+                      <th> AMOUNT </th>
+                      <th> STATUS </th>
+                      <th> ISSUE DATE</th>
+                      <th> action </th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                  <tr className="align-middle"> 
+                    <td> <input type="checkbox" name="" /> 1 </td>
+                    <td> 
+                      <div className="table-product">
+                        <img src={goutam} alt="" />
+                        <div className="product-box">
+                          <div className="tab-content">
+                             <p> Goutam Ray </p>
+                         
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td> goutamr362@gmail.com  </td>
+                    <td> $7,920.00 </td>
+                    <td> <p className="won"> Won Leads </p> </td>
+                    <td> 15/06/2022 14:02</td>
+                    <td>
+                      <button className="eyeBtn" > <FaRegEye /> </button>
+                      <button className="editBtn"> <BiSolidDownload /> </button>
+                      <button className="delBtn">  <FaTrashAlt /> </button>
+                    </td>
+                  </tr>
+
+                  <tr className="align-middle"> 
+                    <td> <input type="checkbox" name="" /> 2 </td>
+                    <td> 
+                      <div className="table-product">
+                        <img src={goutam} alt="" />
+                        <div className="product-box">
+                          <div className="tab-content">
+                             <p> Shanto Ray  </p>
+                        
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td> developergoutam53@gmail.com  </td>
+                    <td> $5,347.00 </td>
+                   
+                    <td><p className="new"> New Leads </p> </td>
+                    <td>15/06/2022 14:02 </td>
+                    <td>
+                      <button className="eyeBtn" > <FaRegEye /> </button>
+                      <button className="editBtn"> <BiSolidDownload /> </button>
+                      <button className="delBtn">  <FaTrashAlt /> </button>
+                    </td>
+                  </tr>
+
+                  <tr className="align-middle"> 
+                    <td> <input type="checkbox" name="" /> 3 </td>
+                    <td> 
+                      <div className="table-product">
+                        <img src={goutam} alt="" />
+                        <div className="product-box">
+                          <div className="tab-content">
+                             <p> Hori Shankor Ray  </p>
+                            
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td> devGoutam@gmail.com </td>
+                    <td> $6,890.00  </td>
+                   
+                    <td><p className="open"> Open Leads </p> </td>
+                    <td> 15/06/2022 14:02</td>
+                    <td>
+                      <button className="eyeBtn" > <FaRegEye /> </button>
+                      <button className="editBtn"> <BiSolidDownload /> </button>
+                      <button className="delBtn">  <FaTrashAlt /> </button>
+                    </td>
+               
+
+                  </tr>
+                  <tr className="align-middle"> 
+                    <td> <input type="checkbox" name="" /> 4 </td>
+                    <td> 
+                      <div className="table-product">
+                        <img src={goutam} alt="" />
+                        <div className="product-box">
+                          <div className="tab-content">
+                             <p> Durjay Ray  </p>
+                   
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td> 	kurulus@gmail.com </td>
+                    <td>$6,872.00  </td>
+                  
+                    <td> <p className="won"> Won Leads </p> </td>
+                    <td> 15/06/2022 14:02 </td>
+                    <td>
+                      <button className="eyeBtn" > <FaRegEye /> </button>
+                      <button className="editBtn"> <BiSolidDownload /> </button>
+                      <button className="delBtn">  <FaTrashAlt /> </button>
+                    </td>             
+                  </tr>
+
+                  <tr className="align-middle"> 
+                    <td> <input type="checkbox" name="" /> 5 </td>
+                    <td> 
+                      <div className="table-product">
+                        <img src={goutam} alt="" />
+                        <div className="product-box">
+                          <div className="tab-content">
+                             <p> Uttam Ray  </p>
+                     
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td> johara@gmail.com </td>
+                    <td> $4,578.00 </td>
+                   
+                    <td> <p className="open"> Open Leads </p> </td>
+                    <td> 15/06/2022 14:02 </td>
+                    <td>
+                      <button className="eyeBtn" > <FaRegEye /> </button>
+                      <button className="editBtn"> <BiSolidDownload /> </button>
+                      <button className="delBtn">  <FaTrashAlt /> </button>
+                    </td>             
+                  </tr>
+
+                  <tr className="align-middle"> 
+                    <td> <input type="checkbox" name="" /> 6 </td>
+                    <td> 
+                      <div className="table-product">
+                        <img src={goutam} alt="" />
+                        <div className="product-box">
+                          <div className="tab-content">
+                             <p> Manik Ray  </p>
+                         
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td> labonno@gmail.com </td>
+                    <td>$4,978.00 </td>        
+                    <td> 
+                      <p className="lost"> Lost Leads </p>
+                    </td>
+                    <td> 15/06/2022 14:02</td>
+                    <td>
+                      <button className="eyeBtn" > <FaRegEye /> </button>
+                      <button className="editBtn"> <BiSolidDownload /> </button>
+                      <button className="delBtn">  <FaTrashAlt /> </button>
+                    </td>              
+                  </tr>
+
+                </tbody>
+              </table>
+              <div className="table-footer">
+                <div className="footer-left">
+                    <p> showing <b> 12 </b>  of <b> 60 </b> results </p>
+                </div>
+                <div className="footer-right">
+                <nav aria-label="...">
+                  <ul className="pagination">
+                    <li className="page-item disabled">
+                      <span className="page-link"> <FaAngleLeft /> </span>
+                    </li>
+                    <li className="page-item active"><a className="page-link" href="#">1</a></li>
+                    <li className="page-item " aria-current="page">
+                      <span className="page-link"> 2 </span>
+                    </li>
+                    <li className="page-item " aria-current="page">
+                      <span className="page-link"> 3 </span>
+                    </li>
+                    <li className="page-item " aria-current="page">
+                      <span className="page-link"> 4 </span>
+                    </li>
+                    <li className="page-item"><a className="page-link" href="#">...</a></li>
+                    <li className="page-item"><a className="page-link" href="#"> 20 </a></li>
+                    <li className="page-item">
+                      <a className="page-link" href="#"> <FaAngleRight /> </a>
+                    </li>
+                  </ul>
+                </nav>
+                </div>
+              </div>
+             </div>
+            </div>
+          </div>
+          </div>
+        </div>
+
+        </div>
+
+   
+
+
     </>
   )
 }
