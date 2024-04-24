ba07342336c7851589122df198a5755ac2f3b7c3
goutam <goutamr362@gmail.com>
2024-04-24 17:41:36 +0600

    user profile & my account page design done

--- src/pages/userAll/myAccount/MyAccount.jsx
+++ src/pages/userAll/myAccount/MyAccount.jsx
@@ -1,17 +1,354 @@
 
-
-
 import BreadCrumb from "../../../components/BreadCrumb/BreadCrumb";
+import goutam from "../../../assets/img/goutam.png"
+import { FaCloudUploadAlt } from "react-icons/fa"; 
+import { MdOutlineVerified } from "react-icons/md";
+import { IoIosLock } from "react-icons/io";
+import { FaEyeSlash } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa6";
+import { MdAddModerator } from "react-icons/md";
+import { MdVerifiedUser } from "react-icons/md";
+
+import { useState } from "react";
+
 import "./MyAccount.css"; 
 const MyAccount = () => {
+   const [activeTab, setActiveTab] = useState(0); 
+   const [open , setOpen] = useState(false)
+   const [open2 , setOpen2] = useState(false)
+   const [open3 , setOpen3] = useState(false)
+
+
+
   return (
     <>
       <div className="right-content">
           <BreadCrumb title={"My Account"} page={"Users"}/> 
 
+       <div className="row my-4">
+        <div className="col-sm-12">
+           <div className="card p-3 shadow tab-content-btn "> 
+              <div className="tab-button">
+                <button className={`  ${activeTab === 0 && "active"} `}  onClick={() => setActiveTab(0)}> Edit Profile </button>
+                <button   className={`  ${activeTab === 1 && "active"} `}  onClick={() => setActiveTab(1)}> Change Password </button>
+                <button  className={`  ${activeTab === 2 && "active"} `}  onClick={() => setActiveTab(2)}> Other Settings </button>
+              </div>
+  
+          {
+            activeTab === 0 && 
+            <div className="edit-profile">
+              <div className="public-private-info p-5">
+                 <div className="public-info">
+                     <h6> Public InFormation </h6>
+                 </div>
+                 <div className="profile-box my-4">
+                    <div className="row">
+                      <div className="col-sm-4">
+                        <div className="profile-photo">
+                          <img src={goutam} alt="" />
+                          <button> <span> <FaCloudUploadAlt /> </span> Upload </button>
+                        </div>
+                      </div>
+                      <div className="col-sm-8">        
+                          <div className="row">
+                            <div className="col-sm-6">                    
+                                <fieldset>
+                                   <legend> FullName </legend>
+                                   <input type="text" placeholder="Type here" className="form-control" defaultValue="Goutam ray"/>
+                                </fieldset>                    
+                            </div>
+                            <div className="col-sm-6">
+                               <fieldset>
+                                   <legend> Username </legend>
+                                   <input type="text" placeholder="Type here" className="form-control" defaultValue="@goutam"/>
+                                </fieldset>
+                            </div>
+                          </div>
+                          <div className="row my-2">                 
+                               <fieldset>
+                                   <legend> Biography </legend>
+                                   <textarea name="" id="" cols="30" rows="6" placeholder="Type here" className="form-control" defaultValue="I am Goutam ray. I am full stack Developer. "></textarea>                           
+                                </fieldset>               
+                          </div>                   
+                      </div>
+                    </div>
+                 </div>
+               </div>
 
 
+              <div className="public-private-info px-5 py-2">
+                 <div className="public-info">
+                     <h6 className="pr-2">Private InFormation </h6>
+                 </div>
+                 <div className="profile-box my-4">
+                    <div className="row">
+                      <div className="col-sm-4">
+                          <fieldset>
+                              <legend> Unique Id </legend>
+                              <input type="text" placeholder="Type here" className="form-control" defaultValue="Goutam ray"/>
+                           </fieldset>   
+                      </div>
+                      <div className="col-sm-4">
+                          <fieldset>
+                              <legend> Role </legend>
+                              <select name="" id="" className="form-select form-control" >
+                                <option value="" selected> select option </option>
+                                <option value="member"> member </option>
+                                <option value="admin"> admin </option>
+                                <option value="vendor"> vendor </option>
+                                <option value="founder"> founder </option>
+                              </select>
+                           </fieldset>   
+                      </div>
+                      <div className="col-sm-4">
+                          <fieldset>
+                              <legend> Status </legend>
+                              <select name="" id="" className="form-select form-control" >
+                                <option value="" selected> select option </option>
+                                <option value="approved"> approved </option>
+                                <option value="pending"> pending </option>
+                                <option value="blocked"> blocked </option>
+                      
+                              </select>
+                           </fieldset>   
+                      </div>                  
+                    </div>
 
+                    <div className="row my-3">
+                      <div className="col-sm-4">
+                          <fieldset>
+                              <legend> Email</legend>
+                              <input type="text" placeholder="Type here" className="form-control" defaultValue="goutamr362@gmail.com"/>
+                           </fieldset>   
+                      </div>
+                      <div className="col-sm-4">
+                          <fieldset>
+                              <legend> Phone </legend>
+                              <input type="text" placeholder="Type here" className="form-control" defaultValue="01755302053"/>
+                           </fieldset>   
+                      </div>
+                      <div className="col-sm-4">
+                          <fieldset>
+                              <legend> Website </legend>
+                              <input type="text" placeholder="Type here" className="form-control" defaultValue="https://developergoutam.com/"/>
+                           </fieldset>   
+                      </div>                  
+                    </div>
+                    <div className="row">
+                      <fieldset>
+                        <legend> Address </legend>
+                        <textarea name="" id="" cols="30" rows="6" defaultValue="Shalbon mistripara , rangpur" className="form-control"></textarea>
+                      </fieldset>
+                    </div>
+
+                 </div>
+               </div>
+
+              <div className="public-private-info px-5 py-2">
+                 <div className="public-info">
+                     <h6 className="pr-2">Social InFormation </h6>
+                 </div>
+                 <div className="profile-box my-4">
+                    <div className="row my-3">
+                      <div className="col-sm-6">
+                          <fieldset>
+                              <legend> Facebook</legend>
+                              <input type="text" placeholder="Type here" className="form-control" defaultValue="https://example.com/"/>
+                           </fieldset>   
+                      </div>
+                      <div className="col-sm-6">
+                          <fieldset>
+                              <legend> Twitter </legend>
+                              <input type="text" placeholder="Type here" className="form-control" defaultValue="https://example.com/"/>
+                           </fieldset>   
+                       </div>
+                       
+                       <div className="row my-3">                                     
+                       <div className="col-sm-6">
+                          <fieldset>
+                              <legend> Linkedin </legend>
+                              <input type="text" placeholder="Type here" className="form-control" defaultValue="https://example.com/"/>
+                           </fieldset>   
+                       </div>                                     
+                       <div className="col-sm-6">
+                          <fieldset>
+                              <legend> Instagram </legend>
+                              <input type="text" placeholder="Type here" className="form-control" defaultValue="https://example.com/"/>
+                           </fieldset>   
+                       </div>                                     
+                      </div>
+
+                       <div className="row my-3">                                     
+                       <div className="col-sm-6">
+                          <fieldset>
+                              <legend> Youtube </legend>
+                              <input type="text" placeholder="Type here" className="form-control" defaultValue="https://example.com/"/>
+                           </fieldset>   
+                       </div>                                     
+                       <div className="col-sm-6">
+                          <fieldset>
+                              <legend> Pinterest </legend>
+                              <input type="text" placeholder="Type here" className="form-control" defaultValue="https://example.com/"/>
+                           </fieldset>   
+                       </div>                                     
+                       </div>
+                    </div>
+
+                 </div>
+                 <button className="btn btn-lg btn-primary save-btn "> <span><MdOutlineVerified /> </span> SAVE CHANGES</button>
+               </div>
+             </div>
+          }
+            
+
+            {
+              activeTab === 1 && 
+              <div className="generate-Password my-5 px-5">
+                <h6> Generate Password</h6>
+                <div className="pass-form">         
+                    <div className="my-2">
+                        <span className="lock"> <IoIosLock />  </span>
+                        <input type="text"  placeholder="current password" className="form-control"/>
+                        <span className="view-hide">
+                          
+                             <button onClick={() => setOpen(!open)} > 
+                              {
+                                 open ?   <FaRegEye /> : <FaEyeSlash />
+                              }
+                             
+                             </button> 
+                          
+                        
+                         </span>
+                     </div>
+                </div>
+                <div className="row">
+                  <div className="col-sm-6">
+                  <div className="pass-form">   
+                     <div className="my-2">
+                        <span className="lock2"> <MdAddModerator />  </span>
+                        <input type="text"  placeholder="new password" className="form-control"/>
+                        <span className="view-hide2">
+                           <button onClick={() => setOpen2(!open2)}> 
+                              {
+                                 open2 ?   <FaRegEye /> : <FaEyeSlash />
+                              }
+                             
+                             </button> 
+                         </span>
+                       </div>
+                     </div>
+                  </div>
+                  <div className="col-sm-6">
+                  <div className="pass-form">   
+                     <div className="my-2">
+                        <span  className="lock2"> <MdVerifiedUser />  </span>
+                        <input type="text"  placeholder="confirm password" className="form-control"/>
+                        <span className="view-hide2">
+                             <button onClick={() => setOpen3(!open3)}> 
+                              {
+                                 open3 ?   <FaRegEye /> : <FaEyeSlash />
+                              }
+                             
+                             </button> 
+                         </span>
+                       </div>
+                     </div>
+                  </div>
+                </div>
+                <button className="btn btn-lg btn-primary save-btn mt-4 mb-0"> <span><MdOutlineVerified /> </span> SAVE CHANGES</button>
+              </div>
+            }
+
+            {
+              activeTab === 2 && 
+             <div className="other-setting px-5 my-5">
+                <div className="row">
+                  
+                  <div className="col-sm-6 ">
+                      <div className="activity-email">
+                        <h6> Activity Email Settings </h6>
+                        <ul>
+                          <li className="mt-4">
+                          <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Someone adds you as a connection </label>
+                          </div>
+                          </li>
+                          <li> 
+                           <div className="form-check form-switch">
+                              <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
+                               <label className="form-check-label" htmlFor="flexSwitchCheckChecked">you re sent a direct message </label>
+                             </div>
+                          </li>
+                          <li> 
+                           <div className="form-check form-switch">
+                              <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
+                               <label className="form-check-label" htmlFor="flexSwitchCheckChecked">New membership approval </label>
+                             </div>
+                          </li>
+                          <li> 
+                           <div className="form-check form-switch">
+                              <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
+                               <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Send Copy To Personal Email </label>
+                             </div>
+                          </li>
+                          <li>
+                          <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Tips on getting more out of PCT-themes </label>
+                          </div>
+                          </li>
+                        </ul>
+                      </div> 
+                  </div>
+                  <div className="col-sm-6 ">
+                      <div className="activity-email">
+                        <h6> Product Email Settings </h6>
+                          <ul>
+                            <li className="mt-4"> 
+                              <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" checked/>
+                                <label className="form-check-label" htmlFor="inlineCheckbox1">Someone adds you as a connection </label>
+                               </div>
+                            </li>
+                            <li> 
+                              <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" checked/>
+                                <label className="form-check-label" htmlFor="inlineCheckbox1"> you re sent a direct message </label>
+                               </div>
+                            </li>
+                            <li> 
+                              <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" checked/>
+                                <label className="form-check-label" htmlFor="inlineCheckbox1"> New membership approval </label>
+                               </div>
+                            </li>
+                            <li> 
+                              <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
+                                <label className="form-check-label" htmlFor="inlineCheckbox1"> Send Copy To Personal Email  </label>
+                               </div>
+                            </li>
+                            <li> 
+                              <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
+                                <label className="form-check-label" htmlFor="inlineCheckbox1"> Tips on getting more out of PCT-themes </label>
+                               </div>
+                            </li>
+                          </ul>
+                      </div> 
+                  </div>
+                </div>
+                <button className="btn btn-lg btn-primary save-btn mt-4 mb-0"> <span><MdOutlineVerified /> </span> SAVE CHANGES</button>
+             </div>
+            }
+           
+           </div>
+        </div>
+       </div>  
+
+ 
 
           
       </div>
--- src/pages/userAll/userProfile/UserProfile.jsx
+++ src/pages/userAll/userProfile/UserProfile.jsx
@@ -1,8 +1,21 @@
 
-
+import { HiDotsHorizontal } from "react-icons/hi"
+import { FaPhoneVolume } from "react-icons/fa6";
+import { IoDocumentTextSharp } from "react-icons/io5"; 
+import { CgWebsite } from "react-icons/cg";
+import { FaAddressCard } from "react-icons/fa6";
+import { FaFacebookF, FaShoppingBag } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { RiWhatsappFill } from "react-icons/ri";
+import { FaYoutube } from "react-icons/fa";
+import { FaStar } from "react-icons/fa6";
+import { FaCartPlus } from "react-icons/fa6";
 
 
 import BreadCrumb from "../../../components/BreadCrumb/BreadCrumb"
+import goutam from "../../../assets/img/goutam.png"
+
 import "./UserProfile.css"
 const UserProfile = () => {
   return (
@@ -11,7 +24,144 @@ const UserProfile = () => {
           <BreadCrumb title={"User Profile"} page={"Users"}/> 
 
 
+        <div className="row my-4">
+          <div className="col-sm-5">
+            <div className="card p-3 shadow profile-left ">
+               <div className="top-part">
+                  <h4> User_information </h4>
+                  <p><HiDotsHorizontal /></p>
+                </div>
+                <div className="user-img-part1">
+                  <img src={goutam} alt="" />
+                  <div className="user-info">
+                    <h4>Goutam Ray  </h4>
+                    <p> Full Stack Developer </p>
+                  </div>
+                </div>
+                <div className="contact-box">
+                  <h6> Communication </h6>
+                  <ul>
+                    <li> <span> <FaPhoneVolume /> </span> 01755302053 </li>
+                    <li> <span> <IoDocumentTextSharp /> </span> goutamr362@gmail.com </li>
+                    <li> <span> <CgWebsite /> </span> developergoutam.com </li>
+                    <li> <span> <FaAddressCard /> </span> Shalbon Mistripara, Rangpur </li>
+                  </ul>
+                </div>
+                <div className="biography my-2">
+                  <h6> Biography </h6>
+                  <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets </p>
+                </div>
 
+                <div className="Elsewhere mt-2">
+                  <h6> Elsewhere </h6>
+                </div>
+                <div className="social-media">
+                  <button className="btn facebook "> <span><FaFacebookF /></span> FaceBook </button>
+                  <button className="btn twitter"><span><FaTwitter /></span> Twitter</button>
+                  <button className="btn linkedin"><span><FaLinkedin /></span> Linkedin </button>
+                  <button className="btn whatsapp"> <span><RiWhatsappFill /></span> WhatsApp </button>
+                  <button className="btn youtube"><span> <FaYoutube /></span> Youtube </button>
+                </div>
+            </div>
+          </div>
+          <div className="col-sm-7">
+            <div className="user-right-part">
+
+              <div className="product-list-topper p-0 mb-4">
+               <div className="row">
+                  <div className="col-sm-4 part1 ">
+                      <div className="total-product ">
+                          <div className="left-top-part ">
+                              <h2> 547 </h2>
+                              <span> <FaCartPlus /> </span>
+                          </div>
+                          <p> total products </p>
+                      </div>
+                  </div>
+                  <div className="col-sm-4 part1 ">
+                      <div className="total-product category ">
+                          <div className="left-top-part ">
+                              <h2> 605 </h2>
+                              <span className="widget"> <FaStar /></span>
+                          </div>
+                          <p> total_categories </p>
+                      </div>
+                  </div>
+                  <div className="col-sm-4 part1 ">
+                      <div className="total-product brand">
+                          <div className="left-top-part ">
+                              <h2> 249 </h2>
+                              <span className="verify"> <FaShoppingBag /> </span>
+                          </div>
+                          <p> total_barnds </p>
+                      </div>
+                  </div>
+                </div>
+              </div>
+           </div>
+              <div className="card p-3 shadow right-part-user ">
+                 <div className="top-part">
+                    <h4> Recent Activities </h4>
+                    <p><HiDotsHorizontal /></p>
+                 </div>
+             <div className="all-data-box">
+                 <div className="user-profile-time">
+                  <ul>
+                    <li>
+                      <div className="all-content">
+                          <div className="user-details-time">
+                            <h6> Your account is logged in </h6>
+                            <p> 45 min ago </p>
+                          </div>
+                          <p> Vestibulum rutrum rutrum neque. Aenean auctor gravida sem quam pede lobortis ligula, sit amet eleifend. </p>
+                          <div className="user-cart-media">
+                            <img src={goutam} alt="" />
+                            <p> Goutam ray </p>
+                          </div>
+                          </div>                    
+                    </li>
+                  </ul>
+                 </div>
+                 <div className="user-profile-time">
+                  <ul>
+                    <li>
+                      <div className="all-content">
+                          <div className="user-details-time">
+                            <h6 className="changed"> Current language has been changed </h6>
+                            <p> 24 hr ago </p>
+                          </div>
+                          <p> Vestibulum rutrum rutrum neque. Aenean auctor gravida sem quam pede lobortis ligula, sit amet eleifend. </p>
+                          <div className="user-cart-media">
+                            <img src={goutam} alt="" />
+                            <p> Goutam ray </p>
+                          </div>
+                          </div>                    
+                    </li>
+                  </ul>
+                 </div>
+                 <div className="user-profile-time">
+                  <ul>
+                    <li>
+                      <div className="all-content">
+                          <div className="user-details-time">
+                            <h6 className="no-product "> Asked about this product</h6>
+                            <p> 5 min ago </p>
+                          </div>
+                          <p> Vestibulum rutrum rutrum neque. Aenean auctor gravida sem quam pede lobortis ligula, sit amet eleifend. </p>
+                          <div className="user-cart-media">
+                            <img src={goutam} alt="" />
+                            <p> Goutam ray </p>
+                          </div>
+                          <button className="btn btn-primary"> GO THERE </button> 
+                       </div>
+                                                         
+                    </li>
+                  </ul>               
+                 </div>
+                 </div>
+              </div>
+          </div>
+        </div>
 
 
           
